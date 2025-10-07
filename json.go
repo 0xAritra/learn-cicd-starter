7a958ddf1e8a7b2c40fb72f939e55f066bf579a8
0xaritra <0xaritra@gmail.com>
2025-10-07 10:13:19 +0530

    fix security issues

--- json.go
+++ json.go
@@ -30,5 +30,9 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	_, err = w.Write(dat)
+
+	if err != nil {
+		http.Error(w, "Failed to write response", http.StatusInternalServerError)
+	}
 }
